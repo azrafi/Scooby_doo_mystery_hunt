de7393201cfd2ba76cc22d79164d7957e7c7ecee
Ifty_2003 <iftyahmed99172@gmail.com>
2025-09-04 21:03:21 +0600

    Update Boost.py

--- Boost.py
+++ Boost.py
@@ -9,3 +9,10 @@ scooby_cd = 10.0
 # Boost becomes available after 1 clue (since we only have 3 total now)
 def boost_available():
     return collected_clues() >= 1 and scooby_cd_left <= 0.0 and not scooby_active
+def activate_boost():
+    global scooby_active, scooby_timer, scooby_cd_left
+    scooby_active = True
+    scooby_timer = scooby_dur
+    scooby_cd_left = scooby_cd
+    # Track achievement
+    achievement_stats['boost_count'] += 1
